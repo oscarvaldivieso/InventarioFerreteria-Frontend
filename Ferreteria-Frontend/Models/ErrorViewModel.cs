c8c6fa7ebdf0997b1ca2850e4b6a2617a850d4a6
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-03-18 12:58:00 -0600

    Create project

---
+++ Ferreteria-Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ferreteria_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
