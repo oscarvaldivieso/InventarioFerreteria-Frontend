9fdb89d3b5e051321ca2c103e2b88d57a68714e0
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-03-18 16:00:26 -0600

    feat(Implement index)

---
+++ Ferreteria-Frontend/Controllers/DepartamentosController.cs
@@ -0,0 +1,29 @@
+﻿using Ferreteria_Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Ferreteria_Frontend.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public DepartamentosController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7214/");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("ListarDepartamento");
+
+            if(response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var departamentos = JsonConvert.DeserializeObject<IEnumerable<DepartamentoViewModel>>(content);
+                return View("Index", departamentos);
+            }   
+            return View(new List<DepartamentoViewModel>());
+        }
+    }
+}
---
+++ Ferreteria-Frontend/Models/DepartamentoViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Ferreteria_Frontend.Models
+{
+    public class DepartamentoViewModel
+    {
+        public string Depa_Codigo { get; set; }
+
+        public string Depa_Descripcion { get; set; }
+
+        public int Usua_Creacion { get; set; }
+
+        public DateTime Feca_Creacion { get; set; }
+
+        public int? Usua_Modificacion { get; set; }
+
+        public DateTime? Feca_Modificacion { get; set; }
+
+        //public virtual tbUsuarios Usua_CreacionNavigation { get; set; }
+
+        //public virtual tbUsuarios Usua_ModificacionNavigation { get; set; }
+
+        //public virtual ICollection<tbMunicipios> tbMunicipios { get; set; } = new List<tbMunicipios>();
+        
+    }
+}
