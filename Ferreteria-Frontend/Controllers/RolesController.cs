a47086881733fc163f255a8d5c6e1db8116cf0b8
Oscar Valdivieso <fernandoscar04@gmail.com>
2025-03-22 19:43:53 -0600

    feat(Roles): Make index design (cards)

---
+++ Ferreteria-Frontend/Controllers/RolesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ferreteria_Frontend.Controllers
+{
+    public class RolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.PageTitle = "Roles";
+            ViewBag.SubTitle = "Acceso";
+            return View();
+        }
+    }
+}
