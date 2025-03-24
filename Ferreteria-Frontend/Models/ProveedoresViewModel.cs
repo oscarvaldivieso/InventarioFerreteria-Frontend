4470a1426f4f76caac52d672ced89af077d4bb80
SOLANGRI69 <ajsl69777@gmail.com>
2025-03-24 11:12:11 -0600

    24-03-2025 Proveedores
    Aún no funciona bien

---
+++ Ferreteria-Frontend/Models/ProveedoresViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ferreteria_Frontend.Models
+{
+    public class ProveedoresViewModel
+    {
+        [Display(Name = "Id")]
+        public int Prov_Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Prov_Nombre { get; set; }
+
+        [Display(Name = "Contacto")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Prov_Contacto { get; set; }
+
+        [Display(Name = "Municipio")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Muni_Codigo { get; set; }
+
+        [Display(Name = "Dirección Exacta")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Prov_DireccionExacta { get; set; }
+
+        public int Usua_Creacion { get; set; }
+
+        public DateTime Feca_Creacion { get; set; }
+
+        public int? Usua_Modificacion { get; set; }
+
+        public DateTime? Feca_Modificacion { get; set; }
+
+        public bool? Prov_Estado { get; set; }
+
+        [NotMapped]
+        public string? UsuaC_Nombre { get; set; }
+
+        [NotMapped]
+        public string? UsuaM_Nombre { get; set; }
+
+        [NotMapped]
+        public string? Muni_Descripcion { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Departamento")]
+        public string? Depa_Codigo { get; set; }
+    }
+}
